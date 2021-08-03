226070a2d0a28e9b023f54cbcd7fc31a21f95fe0
Panagiotis <angelakis@lms.mech.upatras.gr>
2021-08-03 13:48:12 +0300

    test1

--- pcl_serializer/nodes/pcl_deserializer.cpp
+++ pcl_serializer/nodes/pcl_deserializer.cpp
@@ -1,8 +1,11 @@
 #include <chrono>
 #include <cmath>
+#include <integration/PclTransfer.h>
 #include <iostream>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/image_encodings.h>
 #include <vector>
 
 using namespace std::chrono;
@@ -15,10 +18,20 @@ typedef unsigned char uchar;
 class PcDeserializer
 {
 public:
-    PcDeserializer()
+    PcDeserializer() : nh_(""), first_(true)
     {
-        im_depth_sub_ = nh_.subscribe("input_image", 1, &PcDeserializer::im_CB, this);
-        im_depth_pub_ = nh_.advertise<sensor_msgs::Image>("result_image", 0, false);
+        transfer_sub_ = nh_.subscribe("input_pcl_transfer", 1, &PcDeserializer::callback, this);
+        im_pub_ = nh_.advertise<sensor_msgs::CompressedImage>("image_out", 0, false);
+        depth_pub_ = nh_.advertise<sensor_msgs::Image>("depth_out", 0, false);
+        // pcl_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("pcl_out", 0, false);
+    }
+    uint as_uint(const float x)
+    {
+        return *(uint *)&x;
+    }
+    float as_float(const uint x)
+    {
+        return *(float *)&x;
     }
     float half_to_float(const ushort x)
     {                                                                                                                                                        // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
@@ -34,116 +47,44 @@ public:
         const uint m = b & 0x007FFFFF;                                                                                                                                                      // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
         return (b & 0x80000000) >> 16 | (e > 112) * ((((e - 112) << 10) & 0x7C00) | m >> 13) | ((e < 113) & (e > 101)) * ((((0x007FF000 + m) >> (125 - e)) + 1) >> 1) | (e > 143) * 0x7FFF; // sign : normalized : denormalized : saturate
     }
-    uint as_uint(const float x)
+    void callback(const integration::PclTransfer::ConstPtr &msg)
     {
-        return *(uint *)&x;
-    }
-    float as_float(const uint x)
-    {
-        return *(float *)&x;
-    }
-    float bytesToFloat(uchar b0, uchar b1, uchar b2, uchar b3)
-    {
-        float output;
-
-        *((uchar *)(&output) + 0) = b0;
-        *((uchar *)(&output) + 1) = b1;
-        *((uchar *)(&output) + 2) = b2;
-        *((uchar *)(&output) + 3) = b3;
-
-        return output;
-    }
-    float bytesToFloat2(uchar b0, uchar b1)
-    {
-        float output;
-
-        *((uchar *)(&output) + 0) = b0;
-        *((uchar *)(&output) + 1) = b1;
-
-        return output;
-    }
-    void im_CB(const sensor_msgs::Image::ConstPtr &msg)
-    {
-        // if (!first) {
-        //     return;
-        // }
-        depth_compress_msg_.header = msg->header;
-        depth_compress_msg_.width = msg->width;
-        depth_compress_msg_.step = msg->step * 2;
-        depth_compress_msg_.encoding = msg->encoding;
-        depth_compress_msg_.is_bigendian = msg->is_bigendian;
-        depth_compress_msg_.height = msg->height;
-        depth_compress_msg_.width = msg->width;
-        depth_compress_msg_.data.resize(depth_compress_msg_.height * depth_compress_msg_.width * 4);
-
-        //ROS_INFO("Height is %d", msg->height);
-        //ROS_INFO("Width is %d", msg->width);
-        //ROS_INFO("Encoding is %d", msg->encoding);
-        //ROS_INFO("Is bidedian %d", msg->is_bigendian);
-        //ROS_INFO("Step is %d", msg->step);
-        //ROS_INFO("Data Size is %d", msg->data.size());
-
-        for (size_t row = 0; row < msg->height; row++) {
-            for (size_t col = 0; col < msg->width; col++) {
-
-                // uchar b0 = msg->data[row * msg->step + 2 * col + 0];
-                // uchar b1 = msg->data[row * msg->step + 2 * col + 1];
+        if (first_) {
+            depth_image_msg_.header = msg->depth_image.header;
+            depth_image_msg_.width = msg->depth_image.width;
+            depth_image_msg_.step = msg->depth_image.step * 2;
+            depth_image_msg_.encoding = msg->depth_image.encoding;
+            depth_image_msg_.is_bigendian = msg->depth_image.is_bigendian;
+            depth_image_msg_.height = msg->depth_image.height;
+            depth_image_msg_.width = msg->depth_image.width;
+            depth_image_msg_.data.resize(msg->depth_image.height * msg->depth_image.width * 4);
+            first_ = false;
+        }
+        for (size_t row = 0; row < msg->depth_image.height; row++) {
+            for (size_t col = 0; col < msg->depth_image.width; col++) {
                 ushort fs;
-                memcpy((uchar *)(&fs), &msg->data[row * msg->step + 2 * col], 2);
-                // *((uchar *)(&fs) + 0) = b0;
-                // *((uchar *)(&fs) + 1) = b1;
+                memcpy((uchar *)(&fs), &msg->depth_image.data[row * msg->depth_image.step + 2 * col], 2);
                 float d = half_to_float(fs);
-                if (first)
-                    ROS_INFO("The Float at %d %d is %f", row, col, d);
                 d *= 10;
                 if (d == 0 || d > 100)
                     d = std::nanf("");
-                memcpy((uchar *)(&depth_compress_msg_.data[row * depth_compress_msg_.step + 4 * col]), &d, 4);
-                // uchar b00, b01, b02, b03;
-                // b00 = *(uchar *)((&d) + 0);
-                // b01 = *(uchar *)((&d) + 1);
-                // b02 = *(uchar *)((&d) + 2);
-                // b03 = *(uchar *)((&d) + 3);
-                // depth_compress_msg_.data[row * depth_compress_msg_.step + 4 * col + 0] = b00;
-                // depth_compress_msg_.data[row * depth_compress_msg_.step + 4 * col + 1] = b01;
-                // depth_compress_msg_.data[row * depth_compress_msg_.step + 4 * col + 2] = b02;
-                // depth_compress_msg_.data[row * depth_compress_msg_.step + 4 * col + 3] = b03;
+                memcpy((uchar *)(&depth_image_msg_.data[row * depth_image_msg_.step + 4 * col]), &d, 4);
+
             }
         }
-        im_depth_pub_.publish(depth_compress_msg_);
-
-        if (first) {
-            first = false;
-            ROS_INFO("Height is %d", msg->height);
-            ROS_INFO("Width is %d", msg->width);
-            ROS_INFO("Encoding is %d", msg->encoding);
-            ROS_INFO("Is bidedian %d", msg->is_bigendian);
-            ROS_INFO("Step is %d", msg->step);
-            ROS_INFO("Data Size is %d", msg->data.size());
-            for (size_t row = 0; row < msg->height; row++) {
-                for (size_t col = 0; col < msg->width; col++) {
-                    uchar b0 = msg->data[row * msg->step + 2 * col + 0];
-                    uchar b1 = msg->data[row * msg->step + 2 * col + 1];
-                    ushort fs;
-                    *((uchar *)(&fs) + 0) = b0;
-                    *((uchar *)(&fs) + 1) = b1;
-
-                    // if (row == 479 && col == 639) {
-                    // ROS_INFO("The bytes are %d %d", b0, b1);
-                    // float d = half_to_float(fs);
-                    // ROS_INFO("The Float at %d %d is %f", row, col, d);
-                    // }
-                }
-            }
-        }
+        im_pub_.publish(msg->rgb_image);
+        depth_pub_.publish(depth_image_msg_);
     }
 
 protected:
     ros::NodeHandle nh_;
-    ros::Publisher im_depth_pub_;
-    ros::Subscriber im_depth_sub_;
-    sensor_msgs::Image depth_compress_msg_;
-    bool first = true;
+    ros::Publisher im_pub_;
+    ros::Publisher depth_pub_;
+    ros::Publisher pcl_pub_;
+    ros::Subscriber transfer_sub_;
+    sensor_msgs::Image depth_image_msg_;
+    // sensor_msgs::PointCloud2 pcl_msg_;
+    bool first_;
 };
 
 int main(int argc, char **argv)
